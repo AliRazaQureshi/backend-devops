620f09b1c5301cabc579666aa488499bcd843787
Ali Raza Qureshi <47821624+AliRazaQureshi@users.noreply.github.com>
2024-06-07 01:19:26 +0500

    Update courses-routes.ts

--- routes/courses-routes.ts
+++ routes/courses-routes.ts
@@ -1,44 +1,23 @@
 import express from 'express'
-import { Client } from 'pg';
-
-// Configure your PostgreSQL connection here
-const client = new Client({
-    user: 'postgres',
-    host: 'database-1.c9uwe08uospl.us-east-1.rds.amazonaws.com',
-    database: 'postgres',
-    password: 'password1234',
-    port: 5432, // default PostgreSQL port
-});
-
-client.connect();
-
+import { prisma } from '../prisma/db';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    try {
-        const result = await client.query('SELECT * FROM courses');
-        const courses = result.rows;
-        return res.status(200).json({ courses });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
+    const courses = await prisma.course.findMany();
+    return res.json({ courses }).status(200)
+})
+
 
 router.post('/', async (req, res) => {
-    const { title, description } = req.body;
-
-    try {
-        const result = await client.query(
-            'INSERT INTO courses (title, description) VALUES ($1, $2) RETURNING *',
-            [title, description]
-        );
-        const course = result.rows[0];
-        return res.status(201).json({ course });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
+    const body = req.body;
+    console.log({ body })
+    const course = await prisma.course.create({
+        data: {
+            title: body.title,
+            description: body.description
+        }
+    })
+    return res.json({ course }).status(201)
+})
 
 export default router;
